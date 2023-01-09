708c9aa4a56e4f2d9681b71e8610d3f8854e06f1
Silas Vance <phosubvieworsi@gmx.net>
2023-01-09 12:52:00 -0500

    feat: Introduce max history length for memory

---
+++ chatbot_framework/llm_backends/openai.py
@@ -0,0 +1,19 @@
+import openai
+
+class OpenAIBackend:
+    def __init__(self, api_key, model="gpt-3.5-turbo"):
+        openai.api_key = api_key
+        self.model = model
+
+    def generate_response(self, prompt):
+        try:
+            response = openai.ChatCompletion.create(
+                model=self.model,
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            return response.choices[0].message.content
+        except Exception as e:
+            return f"Error from OpenAI: {e}"
+# Simulated change on 2023-01-09 12:52:00
