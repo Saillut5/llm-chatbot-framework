412fd69524ae3be459cb5448ed998c54cc433314
Silas Vance <phosubvieworsi@gmx.net>
2023-01-20 18:41:00 -0500

    feat: Introduce max history length for memory

--- chatbot_framework/memory/__init__.py
+++ chatbot_framework/memory/__init__.py
@@ -8,3 +8,4 @@ __all__ = [
 # Simulated change on 2023-01-04 13:54:00
 # Simulated change on 2023-01-17 15:53:00
 # Simulated change on 2023-01-18 12:59:00
+# Simulated change on 2023-01-20 18:41:00
