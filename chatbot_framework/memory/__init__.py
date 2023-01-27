c3105e0930ec0394ccd003220f1d390edd76f53c
Silas Vance <phosubvieworsi@gmx.net>
2023-01-27 15:42:00 -0500

    refactor: Modularize system prompt handling

--- chatbot_framework/memory/__init__.py
+++ chatbot_framework/memory/__init__.py
@@ -9,3 +9,4 @@ __all__ = [
 # Simulated change on 2023-01-17 15:53:00
 # Simulated change on 2023-01-18 12:59:00
 # Simulated change on 2023-01-20 18:41:00
+# Simulated change on 2023-01-27 15:42:00
