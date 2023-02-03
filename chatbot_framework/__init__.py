75c4eb80a34d0393c246a1fa54888a0a51b9e98c
Silas Vance <phosubvieworsi@gmx.net>
2023-02-03 15:04:00 -0500

    docs: Update README with usage examples

---
+++ chatbot_framework/__init__.py
@@ -0,0 +1,13 @@
+# chatbot_framework/__init__.py
+
+from .chatbot import Chatbot
+from .llm_backends import OpenAIBackend, HuggingFaceBackend
+from .memory import ConversationBufferMemory
+
+__all__ = [
+    "Chatbot",
+    "OpenAIBackend",
+    "HuggingFaceBackend",
+    "ConversationBufferMemory",
+]
+# Simulated change on 2023-02-03 15:04:00
