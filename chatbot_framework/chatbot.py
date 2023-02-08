2f5f05ad7e0996cd3d18976cc24cf102a7023039
Silas Vance <phosubvieworsi@gmx.net>
2023-02-08 17:35:00 -0500

    fix: Handle LLM response errors gracefully

--- chatbot_framework/chatbot.py
+++ chatbot_framework/chatbot.py
@@ -151,3 +151,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-01-27 17:57:00
 # Simulated change on 2023-01-27 18:51:00
 # Simulated change on 2023-01-31 09:52:00
+# Simulated change on 2023-02-08 17:35:00
