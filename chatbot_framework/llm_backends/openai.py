b0ac368b95581896388bf12e8b36ebf44b376503
Silas Vance <phosubvieworsi@gmx.net>
2023-03-06 17:36:00 -0500

    docs: Improve code comments and docstrings

--- chatbot_framework/llm_backends/openai.py
+++ chatbot_framework/llm_backends/openai.py
@@ -22,3 +22,4 @@ class OpenAIBackend:
 # Simulated change on 2023-03-01 12:40:00
 # Simulated change on 2023-03-02 10:19:00
 # Simulated change on 2023-03-06 12:21:00
+# Simulated change on 2023-03-06 17:36:00
