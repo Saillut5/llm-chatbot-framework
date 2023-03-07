97a48599d3287021a2a26b90bf7f7791b1560dcb
Silas Vance <phosubvieworsi@gmx.net>
2023-03-07 09:12:00 -0500

    fix: Handle LLM response errors gracefully

--- chatbot_framework/memory/__init__.py
+++ chatbot_framework/memory/__init__.py
@@ -17,3 +17,4 @@ __all__ = [
 # Simulated change on 2023-02-10 14:04:00
 # Simulated change on 2023-02-21 13:18:00
 # Simulated change on 2023-03-01 18:24:00
+# Simulated change on 2023-03-07 09:12:00
