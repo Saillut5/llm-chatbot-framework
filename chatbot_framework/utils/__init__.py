ca3b06e1723466d3c7ddc800e9aaf1c7641925e2
Silas Vance <phosubvieworsi@gmx.net>
2023-03-31 09:39:00 -0400

    docs: Add contributing guidelines

--- chatbot_framework/utils/__init__.py
+++ chatbot_framework/utils/__init__.py
@@ -8,3 +8,4 @@ __all__ = [
 # Simulated change on 2023-01-10 15:15:00
 # Simulated change on 2023-02-06 17:58:00
 # Simulated change on 2023-02-22 13:01:00
+# Simulated change on 2023-03-31 09:39:00
