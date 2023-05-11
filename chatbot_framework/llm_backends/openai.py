0c980dbe4856e429f057130ce8430a43dc3b8c5e
Silas Vance <phosubvieworsi@gmx.net>
2023-05-11 09:51:00 -0400

    feat: Add support for custom LLM models

--- chatbot_framework/llm_backends/openai.py
+++ chatbot_framework/llm_backends/openai.py
@@ -29,3 +29,4 @@ class OpenAIBackend:
 # Simulated change on 2023-04-18 10:55:00
 # Simulated change on 2023-04-27 17:55:00
 # Simulated change on 2023-05-08 17:14:00
+# Simulated change on 2023-05-11 09:51:00
