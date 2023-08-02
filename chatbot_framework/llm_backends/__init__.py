6fa9fddcb0d45845568b43054555ffa13f040562
Silas Vance <phosubvieworsi@gmx.net>
2023-08-02 13:27:00 -0400

    ci: Setup basic CI/CD with GitHub Actions

--- chatbot_framework/llm_backends/__init__.py
+++ chatbot_framework/llm_backends/__init__.py
@@ -20,3 +20,4 @@ __all__ = [
 # Simulated change on 2023-06-14 09:36:00
 # Simulated change on 2023-06-15 10:52:00
 # Simulated change on 2023-07-18 09:56:00
+# Simulated change on 2023-08-02 13:27:00
