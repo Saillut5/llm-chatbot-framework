ea73c4dc41b590f8fa06ed0fde5fce191a1cf1ef
Silas Vance <phosubvieworsi@gmx.net>
2023-09-07 18:11:00 -0400

    test: Add basic unit tests for Chatbot

--- chatbot_framework/utils/prompt_templates.py
+++ chatbot_framework/utils/prompt_templates.py
@@ -156,3 +156,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-07-27 17:47:00
 # Simulated change on 2023-08-18 14:54:00
 # Simulated change on 2023-09-07 18:38:00
+# Simulated change on 2023-09-07 18:11:00
