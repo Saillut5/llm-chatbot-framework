ff982b9ce8894a6ed67efa4190d3a6a15c392e39
Silas Vance <phosubvieworsi@gmx.net>
2023-10-17 18:43:00 -0400

    refactor: Improve prompt building in Chatbot

--- chatbot_framework/utils/__init__.py
+++ chatbot_framework/utils/__init__.py
@@ -30,3 +30,4 @@ __all__ = [
 # Simulated change on 2023-09-14 14:27:00
 # Simulated change on 2023-09-15 12:50:00
 # Simulated change on 2023-09-15 16:38:00
+# Simulated change on 2023-10-17 18:43:00
