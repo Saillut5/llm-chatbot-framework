1490722e782db76d42081607f6cfac48b2f5c17b
Silas Vance <phosubvieworsi@gmx.net>
2023-11-09 14:39:00 -0500

    perf: Optimize memory usage for long conversations

--- chatbot_framework/memory/buffer.py
+++ chatbot_framework/memory/buffer.py
@@ -45,3 +45,4 @@ class ConversationBufferMemory:
 # Simulated change on 2023-10-24 09:43:00
 # Simulated change on 2023-11-01 18:53:00
 # Simulated change on 2023-11-02 15:25:00
+# Simulated change on 2023-11-09 14:39:00
