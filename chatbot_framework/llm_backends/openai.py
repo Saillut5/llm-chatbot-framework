12193c647696c55cec9ea54fb081ee5666ef24d6
Silas Vance <phosubvieworsi@gmx.net>
2023-12-14 17:03:00 -0500

    feat: Add ConversationBufferMemory

--- chatbot_framework/llm_backends/openai.py
+++ chatbot_framework/llm_backends/openai.py
@@ -48,3 +48,4 @@ class OpenAIBackend:
 # Simulated change on 2023-10-26 18:42:00
 # Simulated change on 2023-10-27 10:16:00
 # Simulated change on 2023-10-27 16:49:00
+# Simulated change on 2023-12-14 17:03:00
