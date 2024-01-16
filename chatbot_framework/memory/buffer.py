5cc30dc4fdf4b550f6ceafa4ab418a470398bc39
Silas Vance <phosubvieworsi@gmx.net>
2024-01-16 10:33:00 -0500

    docs: Add README and project structure

--- chatbot_framework/memory/buffer.py
+++ chatbot_framework/memory/buffer.py
@@ -52,3 +52,4 @@ class ConversationBufferMemory:
 # Simulated change on 2023-12-05 16:45:00
 # Simulated change on 2024-01-10 15:10:00
 # Simulated change on 2024-01-11 13:55:00
+# Simulated change on 2024-01-16 10:33:00
