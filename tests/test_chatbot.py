2d80e3a7f5fc891dbb05db6e81cb766350adfe88
Silas Vance <phosubvieworsi@gmx.net>
2024-01-18 10:14:00 -0500

    feat: Implement HuggingFace backend

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -94,3 +94,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-11-30 16:18:00
 # Simulated change on 2023-12-08 14:02:00
 # Simulated change on 2024-01-15 16:41:00
+# Simulated change on 2024-01-18 10:14:00
