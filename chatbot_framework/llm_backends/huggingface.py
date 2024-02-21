51c0ee31b27d3836ec3e5aaa0a3e086339c6b05f
Silas Vance <phosubvieworsi@gmx.net>
2024-02-21 17:24:00 -0500

    feat: Add support for custom LLM models

--- chatbot_framework/llm_backends/huggingface.py
+++ chatbot_framework/llm_backends/huggingface.py
@@ -168,3 +168,4 @@ if __name__ == "__main__":
 # Simulated change on 2024-01-16 10:19:00
 # Simulated change on 2024-01-26 17:16:00
 # Simulated change on 2024-02-15 14:34:00
+# Simulated change on 2024-02-21 17:24:00
