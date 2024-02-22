7ef86c9cb738bdd161e7926856fe626e62ad426f
Silas Vance <phosubvieworsi@gmx.net>
2024-02-22 16:46:00 -0500

    docs: Add contributing guidelines

--- chatbot_framework/utils/prompt_templates.py
+++ chatbot_framework/utils/prompt_templates.py
@@ -166,3 +166,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-12-12 16:58:00
 # Simulated change on 2024-01-10 14:00:00
 # Simulated change on 2024-01-30 15:06:00
+# Simulated change on 2024-02-22 16:46:00
