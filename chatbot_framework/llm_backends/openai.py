fa4a81b41c6d4488c3339a98f32f08fc021f6e0e
Silas Vance <phosubvieworsi@gmx.net>
2024-02-22 14:08:00 -0500

    feat: Initial chatbot framework setup

--- chatbot_framework/llm_backends/openai.py
+++ chatbot_framework/llm_backends/openai.py
@@ -53,3 +53,4 @@ class OpenAIBackend:
 # Simulated change on 2024-01-08 17:17:00
 # Simulated change on 2024-01-24 12:22:00
 # Simulated change on 2024-02-05 15:27:00
+# Simulated change on 2024-02-22 14:08:00
