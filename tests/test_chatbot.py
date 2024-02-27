d0cb84e9189e8ded54611080f5d1b0c56d3a6ec5
Silas Vance <phosubvieworsi@gmx.net>
2024-02-27 12:16:00 -0500

    docs: Add README and project structure

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -101,3 +101,4 @@ if __name__ == "__main__":
 # Simulated change on 2024-02-19 18:27:00
 # Simulated change on 2024-02-23 11:18:00
 # Simulated change on 2024-02-26 13:23:00
+# Simulated change on 2024-02-27 12:16:00
