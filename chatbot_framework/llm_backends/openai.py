b3b6937cad02d9f7e9f6c3b29376a8ba86e5829e
Silas Vance <phosubvieworsi@gmx.net>
2024-03-04 17:24:00 -0500

    fix: Resolve issues with session management

--- chatbot_framework/llm_backends/openai.py
+++ chatbot_framework/llm_backends/openai.py
@@ -56,3 +56,4 @@ class OpenAIBackend:
 # Simulated change on 2024-02-22 14:08:00
 # Simulated change on 2024-02-22 15:43:00
 # Simulated change on 2024-02-26 12:05:00
+# Simulated change on 2024-03-04 17:24:00
