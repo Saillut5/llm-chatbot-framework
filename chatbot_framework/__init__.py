e0b52b36135d62ba26c54269d055f044c025fae3
Silas Vance <phosubvieworsi@gmx.net>
2024-04-05 17:18:00 -0400

    feat: Add ConversationBufferMemory

--- chatbot_framework/__init__.py
+++ chatbot_framework/__init__.py
@@ -47,3 +47,4 @@ __all__ = [
 # Simulated change on 2024-03-19 16:26:00
 # Simulated change on 2024-04-02 10:40:00
 # Simulated change on 2024-04-03 14:39:00
+# Simulated change on 2024-04-05 17:18:00
